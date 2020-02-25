8d3b17c321260ef8d9ebd162eba5db16a4bfe225
Suraj Rampure <surajr@me.com>
2020-02-25 00:49:24 -0800

    release lab4

---
+++ lab/lab04/tests/q2c.py
@@ -0,0 +1,13 @@
+test = {   'name': 'q2c',
+    'points': 1,
+    'suites': [   {   'cases': [   {   'code': '>>> '
+                                               'np.isclose(kde(gaussian_kernel, '
+                                               '1.0, 2.0, np.array([3.0, 4.0, '
+                                               '5.0, 7.0])), 0.075099)\n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
