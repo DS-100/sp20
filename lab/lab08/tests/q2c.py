d0e237f2f0647e25670caaf974847e0acc3c4e4a
Suraj Rampure <surajr@me.com>
2020-04-07 13:50:30 -0400

    release lab08

---
+++ lab/lab08/tests/q2c.py
@@ -0,0 +1,13 @@
+test = {   'name': 'q2c',
+    'points': 1,
+    'suites': [   {   'cases': [   {   'code': '>>> '
+                                               'np.isclose(best_alpha_ridge, '
+                                               '0.15830508474576271, atol = '
+                                               '0.01)\n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ lab/lab08/tests/q4.py
@@ -0,0 +1,12 @@
+test = {   'name': 'q4',
+    'points': 1,
+    'suites': [   {   'cases': [   {   'code': '>>> np.isclose(test_RMSE, '
+                                               '4.994121608671195, atol = '
+                                               '0.01)\n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
