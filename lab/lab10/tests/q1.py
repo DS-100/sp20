1c733a09f035928539562c90578cc05dd3c26ba7
Kunal Agarwal <kagarwal2@berkeley.edu>
2020-04-21 01:35:52 -0700

    release lab10

---
+++ lab/lab10/tests/q1.py
@@ -0,0 +1,21 @@
+test = {   'name': 'q1',
+    'points': 1,
+    'suites': [   {   'cases': [   {   'code': '>>> sum(lr.predict(x_test) == '
+                                               'y_test) == 130\n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': '>>> np.isclose(train_accuracy, '
+                                               '0.8709, atol=1e-4)\n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': '>>> np.isclose(test_accuracy, '
+                                               '0.9091, atol=1e-4)\n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ lab/lab10/tests/q3.py
@@ -0,0 +1,23 @@
+test = {   'name': 'q3',
+    'points': 1,
+    'suites': [   {   'cases': [   {   'code': '>>> '
+                                               'np.isclose(lr_loss(np.array([0, '
+                                               '0]), Phi, Y), '
+                                               '0.69314718055994529)\n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': '>>> np.isclose(lr_loss(theta,\n'
+                                               '...                           '
+                                               'Phi,\n'
+                                               '...                           '
+                                               'Y),\n'
+                                               '...                   '
+                                               '0.29120889606613115)\n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
